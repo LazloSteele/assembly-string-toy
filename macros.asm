f8d70d58276b31d9fac3ad03295e7fcd4b95143f
Lazlo Steele <lazlosteele@gmail.com>
2024-10-14 22:07:33 -0600

    replaced output buffer with stack allocation

--- macros.asm
+++ macros.asm
@@ -51,6 +51,34 @@
 		bne $t2, 10, upper_loop
 .end_macro
 ####################################################################################################
+# macro: alpha
+# purpose: to make printing messages more eloquent
+# registers used:
+#	$t0 - string to check for upper case
+#	$t1 - ascii 'a', 'A'-'Z' is all lower value than 'a'
+# variables used:
+#	%message - message to be printed
+####################################################################################################
+.macro alpha (%char)
+	move $t2, %char
+	li $t8, 0
+	
+	blt $t2, 'A', invalid		# If byte is less than ascii 'A' then it's not alphabetic
+	bgt $t2, 'Z', mebbe		# If byte is greater than 'Z' then maybe valid as a lower but also maybe punctuation
+	j valid				#
+
+	mebbe:				#
+		blt $t2, 'a', invalid	# If byte is less than ascii 'a' it is invalid
+		bgt $t2, 'z', invalid	# If byte is greater than ascii 'z' then it's not alphabetic
+		j valid
+	valid:
+		li $t8, 1
+		j alpha_end		# iterate!
+	invalid:			#
+		j alpha_end		# do nothing and iterate!
+	alpha_end:
+.end_macro
+####################################################################################################
 # function: again
 # purpose: to user to repeat or close the program
 # registers used:
--- main.asm
+++ main.asm
@@ -26,7 +26,7 @@
 	newline: .asciiz "\n"
 	.align 2
 	buffer: .space 100
-	output_buffer: .space 100
+	condensed_length: .word 0
 
 .include "macros.asm"
 
@@ -38,51 +38,67 @@ main:
 	read_str (buffer, 101)
 	
 	la $a0, buffer
-	jal white_space_wipeout
+	jal condensed_to_stack
 	
-	la $a0, output_buffer
+	reset_buffer (buffer, 101)
+	la $a0, buffer
+	jal stack_to_buffer
+	
 	jal print_condensed
 
-	reset_buffer (output_buffer, 101)	# reset output buffer
-	j again_loop
+	again_loop:
+		again
+		j again_loop
 	
-again_loop:
-	again
-	j again_loop
-
-white_space_wipeout:
+#################################
+condensed_to_stack:
 	move $s0, $ra
 	move $t0, $a0
-	la $t1, output_buffer			#
+	
+	subu $sp, $sp, 100		# free up 100 characters (bytes) on the stack
+	move $t1, $sp
+
 	for_char:				#
 	   	lb $t2, 0($t0)   		# Load byte from buffer
-    	beq $t2, 10, done   		# '\n' or 0a [HEX] or 10 [DEC] denotes end of string, if end of string is reached without error then input is valid!
-		blt $t2, 'A', invalid		# If byte is less than ascii 'A' then it's not alphabetic
-		bgt $t2, 'Z', mebbe		# If byte is greater than 'Z' then maybe valid as a lower but also maybe punctuation
-		j valid				#
-		mebbe:				#
-			blt $t2, 'a', invalid	# If byte is less than ascii 'a' it is invalid
-	       		bgt $t2, 'z', invalid	# If byte is greater than ascii 'z' then it's not alphabetic
-	       		j valid
-		valid:
-			sb $t2, 0($t1)       	# store the character in the output buffer
+    	beq $t2, 10, done 		# '\n' or 0a [HEX] or 10 [DEC] denotes end of string, if end of string is reached without error then input is valid!
+
+		alpha ($t2)
+		beqz $t8, iterate
+
+		store_char:
+			sb $t2, 0($t1)       	# store the character in the stack
 			addi $t1, $t1, 1	# move the address up one to store the next character
-			j iterate		# iterate!
-		invalid:			#
-			j iterate		# do nothing and iterate!
 		iterate:			#
 			addi $t0, $t0, 1	# yo dawg I hear you like iterating so we iterate the iterator			
-    			j for_char		# so you can iterate... the function...	
+    		j for_char		# so you can iterate... the function...	
 	done:
 		move $ra, $s0
 		jr $ra
-
-print_condensed:
+#################################
+stack_to_buffer:
 	move $s0, $ra
 	move $t0, $a0
+	move $t1, $sp
+
+	for_char_in_stack:				#
+	   	lb $t2, 0($t1)   		# Load byte from buffer
+    	beq $t2, 0, stack_done 		# '\n' or 0a [HEX] or 10 [DEC] denotes end of string, if end of string is reached without error then input is valid!
+
+		sb $t2, 0($t0)       	# store the character in the buffer
+		addi $t1, $t1, 1	# move the address up one to store the next character
+		addi $t0, $t0, 1
+    	j for_char_in_stack		# so you can iterate... the function...	
+
+	stack_done:
+		move $ra, $s0
+		jr $ra
+
+#################################
+print_condensed:
+	move $s0, $ra
 	
 	print_str (output_msg) # Store the message to format the output
-	print_str (output_buffer)			# load the output buffer to be printed
+	print_str (buffer)			# load the output buffer to be printed
 	print_str (newline)
 	
 	move $ra, $s0
